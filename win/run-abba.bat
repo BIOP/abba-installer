52435c65338de57cd096cce21fb29f23fd482618
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-01-27 11:30:02 +0100

    fix issue with maven not being in PATH

--- win/run-abba.bat
+++ win/run-abba.bat
@@ -16,6 +16,8 @@ call !CONDA_ENV_PATH!\condabin\activate.bat
 :: Set the environment variables related to Java
 set "JAVA_HOME=!CONDA_ENV_PATH!\Library\jre"
 set "PATH=!CONDA_ENV_PATH!\Library\jre\bin;!PATH!"
+:: line necessary or maven will not be in PATH
+set "PATH=!CONDA_ENV_PATH!\Library\bin;!PATH!"
 
 :: Verify Java version and environment
 echo Activated Conda environment at: !CONDA_ENV_PATH!
